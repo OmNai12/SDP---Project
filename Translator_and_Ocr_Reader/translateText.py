92c80fcde960cb2b2ba856f02cc9f6712e405a16
Om Nai <93648778+OmNai12@users.noreply.github.com>
2023-04-12 21:20:39 +0530

    Done

--- Translator_and_Ocr_Reader/translateText.py
+++ Translator_and_Ocr_Reader/translateText.py
@@ -1,4 +1,5 @@
 from googletrans import Translator
+# import gtts
 
 
 def translatorFunction(sent: str):
@@ -6,6 +7,8 @@ def translatorFunction(sent: str):
     res = translator.detect(sent)
     if res.lang == 'en':
         outputText = translator.translate(sent, dest='gu')
+        # speakLang = gtts.gTTS(outputText, lang='gu')
     else:
         outputText = translator.translate(sent, dest='en')
+        # speakLang = gtts.gTTS(outputText, lang='en')
     return outputText.text
